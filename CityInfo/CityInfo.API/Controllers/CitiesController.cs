b8d3c6b2ca69c48195050bba7baf0b144f625bfa
steven <steven@teamgecko.com>
2021-01-19 22:47:22 -0500

    Add routing as well as models to transmit the data back to the user (DTO models)

---
+++ CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(
+                CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
+        
+        }
+    }
+}
--- CityInfo/CityInfo.API/Startup.cs
+++ CityInfo/CityInfo.API/Startup.cs
@@ -16,6 +16,9 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddMvc(); // Use the below 5.0 uses endpoint routing doing this to follow demo
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,15 +33,18 @@ namespace CityInfo.API
                 app.UseExceptionHandler();
             }
 
-            app.UseRouting();
+            app.UseMvc();
+            
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async  context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            //app.UseRouting();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async  context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
         }
     }
 }
